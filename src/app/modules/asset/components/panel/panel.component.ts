7e165ec652ce042fcfd9c624b1b5d5037d77b4de
Marcelo Galvao <marcelo.galvao.rosal@avanade.com>
2023-03-21 17:51:11 -0300

    consumindo dados da api

--- src/app/modules/asset/components/panel/panel.component.ts
+++ src/app/modules/asset/components/panel/panel.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { YahooResponse } from '../../models/yahoo.interface';
+import { YahooService } from '../../services/yahoo.service';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss'],
 })
-export class PanelComponent {}
+export class PanelComponent implements OnInit {
+  public quotes: YahooResponse[] = [];
+
+  constructor(private yahooService: YahooService) {}
+
+  ngOnInit(): void {
+    this.getPetra4();
+  }
+
+  private getPetra4() {
+    this.yahooService.getPetra4().subscribe(
+      (res: YahooResponse[]) => {
+        this.quotes = this.handleData(res);
+      },
+      (error) => {
+        this.quotes = [];
+      }
+    );
+  }
+
+  handleData(data: YahooResponse[]) {
+    data.reverse();
+
+    // primeiro dia
+    const openFirstDay: any = parseFloat(data[0].open).toFixed(2);
+
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+
+      if (item.open === null) continue;
+      // data
+      // formattedData.date = new Date(formattedData.timestamp);
+
+      // valor
+      item.open = parseFloat(item.open).toFixed(2);
+
+      // diferencas
+      if (index === 0) {
+        item.differenceD1 = '-';
+        item.diffecenceFirstDay = '-';
+      } else {
+        const d1 = data[index - 1].open;
+        item.differenceD1 = (item.open - d1).toFixed(2);
+        item.diffecenceFirstDay = (item.open - openFirstDay).toFixed(2);
+      }
+    }
+
+    return data;
+  }
+}
---
+++ src/app/modules/asset/models/yahoo.interface.ts
@@ -0,0 +1,12 @@
+export interface YahooResponse {
+  date: string;
+  open: any;
+  high: number;
+  low: number;
+  close: number;
+  adjClose: number;
+  volume: number;
+  symbol: string;
+  differenceD1: number | string;
+  diffecenceFirstDay: number | string;
+}
