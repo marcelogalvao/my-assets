7a1ae411a27ead3db937d53e95a63b28ff8dcc51
Marcelo Galvao <marcelo.galvao.rosal@avanade.com>
2023-03-22 12:46:10 -0300

    base dos testes unitarios

--- src/app/modules/asset/asset.component.spec.ts
+++ src/app/modules/asset/asset.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AssetComponent } from './asset.component';
+import { YahooService } from './services/yahoo.service';
+import { of } from 'rxjs';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+const YahooServiceStub = {
+  getPetra4: () => of({}),
+};
 
 describe('AssetComponent', () => {
   let component: AssetComponent;
@@ -8,6 +18,9 @@ describe('AssetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule, SharedModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: YahooService, useValue: YahooServiceStub }],
       declarations: [AssetComponent],
     }).compileComponents();
 
--- src/app/modules/asset/components/panel/panel.component.spec.ts
+++ src/app/modules/asset/components/panel/panel.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { PanelComponent } from './panel.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('PanelComponent', () => {
   let component: PanelComponent;
@@ -8,6 +13,13 @@ describe('PanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        SharedModule,
+        FontAwesomeModule,
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [PanelComponent],
     }).compileComponents();
 
