1056776a3d84fb51e7d1274bd053cd6c63e84c2d
Marcelo Galvao <marcelo.galvao.rosal@avanade.com>
2023-03-22 14:50:36 -0300

    teste unitario

--- src/app/modules/asset/components/chart/chart.component.spec.ts
+++ src/app/modules/asset/components/chart/chart.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartComponent } from './chart.component';
 
+const quotes = [
+  { date: '2023-01-01', open: 1.0, symbol: 'test' },
+  { date: '2023-01-01', open: 1.0, symbol: 'test' },
+];
+
 describe('ChartComponent', () => {
   let component: ChartComponent;
   let fixture: ComponentFixture<ChartComponent>;
@@ -18,5 +23,13 @@ describe('ChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test loadChart()', () => {
+    component.quotes = quotes;
+    component.ngOnInit();
+
+    expect(component.barChartData.datasets[0].data.length).toEqual(2);
+    expect(component.barChartData?.labels?.length).toEqual(2);
   });
 });
--- src/app/modules/asset/components/panel/panel.component.spec.ts
+++ src/app/modules/asset/components/panel/panel.component.spec.ts
@@ -6,6 +6,16 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { PanelComponent } from './panel.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { of } from 'rxjs';
+import { YahooService } from '../../services/yahoo.service';
+
+const YahooServiceStub = {
+  getPetra4: () =>
+    of([
+      { date: '2023-01-01', open: 1.0, symbol: 'test' },
+      { date: '2023-01-01', open: 1.0, symbol: 'test' },
+    ]),
+};
 
 describe('PanelComponent', () => {
   let component: PanelComponent;
@@ -20,6 +30,7 @@ describe('PanelComponent', () => {
         FontAwesomeModule,
       ],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: YahooService, useValue: YahooServiceStub }],
       declarations: [PanelComponent],
     }).compileComponents();
 
@@ -30,5 +41,10 @@ describe('PanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test getPetra4()', () => {
+    component.ngOnInit();
+    expect(component.quotes.length).toEqual(2);
   });
 });
